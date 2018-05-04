e4dfb454b7626bbb60a4168762a4f8976d71ea1f
Lzu Tao <1stpangu@gmail.com>
2018-05-04 10:24:40 +0700

    various changes: * new easy to use autogen.sh * remove obsolete intltool * move to modern Gettext v0.19.7+ * modern autoconf v2.69 * non-recursive Makefile.am

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,46 @@
 #!/bin/sh
-set -e
-set -x
+# Run this to generate all the initial makefiles, etc.
+# Ref: https://github.com/GNOME/gtetrinet/blob/master/autogen.sh
 
-autopoint --force
-intltoolize --automake --copy --force
-libtoolize --automake --copy
-aclocal -I m4
-autoheader
-automake --add-missing --copy
-autoconf
-export CFLAGS="-g -O0"
-export CXXFLAGS="$CFLAGS"
-./configure --enable-maintainer-mode $*
+# set -e
+# set -x
+
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
+
+olddir=$(pwd)
+cd "$srcdir"
+
+( test -f configure.ac ) || {
+    echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
+    exit 1
+}
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+    echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+    echo "*** If you wish to pass any to it, please specify them on the" >&2
+    echo "*** '$0' command line." >&2
+    echo "" >&2
+fi
+
+AUTORECONF=$(which autoreconf)
+if test -z "$AUTORECONF"; then
+    echo "*** No autoreconf found, please intall it ***"
+    exit 1
+fi
+
+autoreconf --force --install --verbose || exit 1
+
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+    # "$srcdir/configure" --enable-maintainer-mode "$@" || exit 1
+    "$srcdir/configure" "$@" || exit 1
+
+    if test "$1" = "--help"; then
+        exit 0
+    else
+        echo "Now type 'make' to compile."
+    fi
+else
+    echo "Skipping configure process."
+fi
